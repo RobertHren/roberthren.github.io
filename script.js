814289b2f8304fb1e7f50edfe26b6b3d7cc1075d
Robert Dev <robert.skrlec@gmail.com>
2014-09-02 14:56:33 +0200

    pa2

---
+++ script.js
@@ -0,0 +1,84 @@
+angular.module('ionicApp', ['ionic'])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('tabs', {
+      url: "/tab",
+      abstract: true,
+      templateUrl: "tabs.html"
+    })
+    .state('tabs.home', {
+      url: "/home",
+      views: {
+        'home-tab': {
+          templateUrl: "home.html",
+          controller: 'HomeTabCtrl'
+        }
+      }
+    })
+    .state('tabs.info', {
+      url: "/info",
+      views: {
+        'home-tab': {
+          templateUrl: "info.html"
+        }
+      }
+    })
+    .state('tabs.review', {
+      url: "/review",
+      views: {
+        'home-tab': {
+          templateUrl: "review.html"
+        }
+      }
+    })
+    .state('tabs.about', {
+      url: "/about",
+      views: {
+        'about-tab': {
+          templateUrl: "about.html"
+        }
+      }
+    })
+    .state('tabs.share', {
+      url: "/share",
+      views: {
+        'home-tab': {
+          templateUrl: "share.html"
+        }
+      }
+    })
+    .state('tabs.review-happy', {
+      url: "/review-happy",
+      views: {
+        'home-tab': {
+          templateUrl: "review-happy.html"
+        }
+      }
+    })
+    .state('tabs.review-sad', {
+      url: "/review-sad",
+      views: {
+        'home-tab': {
+          templateUrl: "review-sad.html"
+        }
+      }
+    })
+    .state('tabs.contact', {
+      url: "/contact",
+      views: {
+        'contact-tab': {
+          templateUrl: "contact.html"
+        }
+      }
+    });
+
+
+  $urlRouterProvider.otherwise("/tab/home");
+
+})
+
+.controller('HomeTabCtrl', function ($scope) {
+  console.log('HomeTabCtrl');
+});
