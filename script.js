d26881ddd1765c1e2915425e25dc6f3b62abe82d
Robert Dev <robert.skrlec@gmail.com>
2014-09-02 20:27:58 +0200

    pa16

--- script.js
+++ script.js
@@ -87,37 +87,3 @@ angular.module('ionicApp', ['ionic'])
 
 })
 
-function handleSocialShare()
-{
-    $('#select-choice-share option:selected').each(function()
-    {
-        text = "Flash vs HTML5 Trendanalyse";
-        url ="http://www.sebastianviereck.de/flash-html5-trendanalyse/#.ULTEkYb9n2A";
-
-        shareService = $(this).val()
-        switch (shareService) {
-            case "facebook":
-                shareFacebookLike(url);
-                break;
-            case "twitter":
-                shareTwitter(url, text);
-                break;
-            case "email":
-                shareEmail(url, text);
-                break;
-            default:
-
-        }
-    });
-}
-
-function shareEmail(subject, body)
-{
-    window.location = "mailto:&subject=" + subject + "&body=" + body;
-}
-function shareTwitter(url, text)
-{
-    window.location = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(text) + "&url=" + encodeURIComponent(url);
-}
-
-
